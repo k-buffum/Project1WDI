7d0e715b2867a9cef35edee8ea85c05458726f70
k-buffum <knbuffum@gmail.com>
2016-02-03 14:51:06 -0800

    Added media styling to hangman page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-	
+	var instructions = 0;
+	$('#instructionsContainer').hide();
+
+	$('#instructions').on('click', function() {
+		if(instructions == 0) {
+			$('#instructionsContainer').show();
+			instructions = 1;
+		} else {
+			$('#instructionsContainer').hide();
+			instructions = 0;
+		}
+		
+	})	
 })
